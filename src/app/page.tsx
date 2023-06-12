2a25e3331270376d61ada9a440a37ce1555333cc
crazywook <wook9898@hanmail.net>
2023-06-13 00:13:22 +0900

    🎉 init

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default async function Home() {
+
+  return (
+    <>
+      <h1>Hello, Next.js 13 App Directory!</h1>
+      <p>
+        <Link href="/initial-data">Prefetching Using initial data</Link>
+      </p>
+      <p>
+        <Link href="/hydration">Prefetching Using Hydration</Link>
+      </p>
+    </>
+  );
+
+}
